2c29a352d4e09f4fd33c3f8ee3561227fbd84955
Sandi Junker <sandi.junker@outlook.com>
2025-01-14 18:34:54 -0600

    Set up dotenv package

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 //Express module
 let express = require("express");
 let app = express();
+const env = require("dotenv").config();
 
 //Use the router for home page
 app.use("/", require("./routes"));
 
 //Server
-app.listen(3000, () => {
-    console.log("Running on http://localhost:3000");
-})
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`);
+});
