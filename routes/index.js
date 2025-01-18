ebebd5b453bde9d19ead93f8203677e257662b88
Sandi Junker <sandi.junker@outlook.com>
2025-01-18 11:13:16 -0600

    Re-add home stuff

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ let router = express.Router();
 //Get contacts routes
 router.use('/contacts', require('./contacts'));
 
-/*Get controller
+//Get controller
 let controller = require("../controllers");
 
 //Get res for home page
-router.get("/", controller.getName);*/
+router.get("/", controller.getName);
 
 //Export the router
 module.exports = router;
