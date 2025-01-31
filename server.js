d690cb94bb3a72c5e07f5f296b23b130ab04716f
Sandi Junker <sandi.junker@outlook.com>
2025-01-31 10:59:56 -0600

    Fix swagger

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const port = process.env.PORT || 3000;
 
 //Use the router for home page
 app.use(bodyParser.json())
-    /*.use((req, res, next) => {
+    .use((req, res, next) => {
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader(
+        /*res.setHeader(
             "Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
         );
         res.setHeader("Content-Type", "application/json");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");*/
         next();
-    })*/
+    })
     .use("/", require("./routes"));
 
 //Start db
