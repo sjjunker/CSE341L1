54caae3a9d7ed68c43c8e3e99ea3ad39fd943461
Sandi Junker <sandi.junker@outlook.com>
2025-01-31 10:08:30 -0600

    Change "Access-Controll-Allow_Methods" to "Access-Controll-Allow-Methods"

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
             "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
         );
         res.setHeader("Content-Type", "application/json");
-        res.setHeader("Access-Controll-Allow_Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.setHeader("Access-Controll-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         next();
     })
     .use("/", require("./routes"));
