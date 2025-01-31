d537940e9cc0d4e1efc1aceaf3d8ed14dd8d49f1
Sandi Junker <sandi.junker@outlook.com>
2025-01-31 10:55:40 -0600

    Trying to fix swagger css

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
             "Access-Control-Allow-Headers",
             "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
         );
-        res.setHeader("Content-Type", "application/json");
+        //res.setHeader("Content-Type", "application/json");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         next();
     })
